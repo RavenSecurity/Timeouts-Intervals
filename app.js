345d1b3b54886b10b62af327b23fbe406a6795ae
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-05-03 11:50:48 +0200

    my first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+
+    // Combining timeouts and events
+
+function theExplosion() {
+    alert("BOOM!");
+  }
+  
+  const button = document.createElement("button");
+  button.textContent = "WARNING";
+  button.addEventListener("click", () => {
+    // this will delay the event by 5 seconds
+    setTimeout(theExplosion, 5000);
+  });
+  document.body.appendChild(button);
+}
+
+    // Recursion
+
+let times = 0;
+function heyYou() {
+  console.log("Hey! You! I called you " + times + " times!");
+  times++;
+
+  const nextCall = Math.floor(Math.random() * 2000); // Random delay until heyYou() is called again
+  setTimeout(heyYou, nextCall);
+}
